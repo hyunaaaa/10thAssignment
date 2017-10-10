b7abd12fd8b559a110ee85a000357ff2e5dee14a
hyunaaaa <31638940+hyunaaaa@users.noreply.github.com>
2017-10-10 21:16:04 +0900

    MyFrame1

---
+++ MyFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		setSize(300, 200);
+		setLocation(screenSize.width/2, screenSize.height/2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("MyFrame");
+		Image img = kit.getImage("icon.gif");
+		setIconImage(img);
+		setLayout(new FlowLayout());
+		JButton button = new JButton("버튼");
+		this.add(button);
+		setVisible(true);
+	}
+}
